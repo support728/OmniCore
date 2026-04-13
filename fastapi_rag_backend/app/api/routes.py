e0d80934e33163732eeac50107b3ff91283b5ad9
Saye Monibah <support@omni-core-ai.com>
2026-04-12 21:07:04 -0500

    fix: clean backend structure

--- fastapi_rag_backend/app/api/routes.py
+++
@@ -1,9 +0,0 @@
-def detect_route(request):
-    text = request.message.lower()
-
-    if "weather" in text:
-        return "weather"
-    elif "news" in text:
-        return "news"
-    else:
-        return "general"
